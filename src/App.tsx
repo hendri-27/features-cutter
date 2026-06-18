6c6326d113ffd4ba43eb2496b339e01c2666ceed
Hendri Antonius <hendriantonius27@gmail.com>
2026-06-18 11:38:16 +0700

    feat(shadcn/ui): install and configure shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Features Cutter AI</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Features Cutter AI</h1>
+      <Button>Test</Button>
+    </>
+  );
 }
 
 export default App;
